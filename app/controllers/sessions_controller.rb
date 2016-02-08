a6334d2bdfed14e66cb5d6fb939ca8697773336d
Justin Thomas <jthomas0209@gmail.com>
2016-02-08 13:49:40 -0500

    Done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete(:name) if session[:name]
+    redirect_to login_path
+  end
+
+  private
+    def current_user
+      session[:name]
+    end
+end
