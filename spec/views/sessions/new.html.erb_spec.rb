75b13a50e67b02dc491c46e6f6f18441e5185c28
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-08 17:37:58 -0700

    Done.

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "sessions/new.html.erb", type: :view do
+RSpec.describe "sessions/new.html.erb", type: :feature do
   # pending "add some examples to (or delete) #{__FILE__}"
+
   context "title" do
+    it "says 'Enter your username to login'" do
+      visit '/sessions/new'
+      expect(page).to have_content('Enter your username to login')
+    end
+  end
+
+  context "user navigation" do
     it 'submits a username' do
       visit '/sessions/new'
       fill_in('name', :with => 'John')
       click_button 'login'
+      expect(page).to have_content('John')
     end
   end
 end
