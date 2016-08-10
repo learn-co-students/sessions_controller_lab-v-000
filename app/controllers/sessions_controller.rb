56b34f390f4b42ef9fea8de7b6c3de1def875cde
Ann Lee <aplee29@users.noreply.github.com>
2016-08-10 06:18:00 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      # redirect_to root_path
+      redirect_to '/'
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name
+    end
+    redirect_to login_path
+  end
+
+end
