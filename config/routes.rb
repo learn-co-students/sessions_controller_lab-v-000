1b9e045ab307f47d5453e4eb2561e790a707c35a
Marcel Galang <marcelgalang@gmail.com>
2016-10-24 18:30:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
+  get 'sessions/create'
+
   root 'application#hello'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
