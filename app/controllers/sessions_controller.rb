e53178db189dc027d12d878d2339dce7dbf78dff
yeungn <nin.yeung@gmail.com>
2017-01-05 16:35:36 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/login'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+end
