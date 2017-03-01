424c15029b38013f605810e0f96d7eeb768ad71f
Jon Fougner <jon.fougner@gmail.com>
2017-03-01 22:24:26 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def create
+    return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to '/'
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to(controller: 'sessions', action: 'new')
+  end
+
+end
