858830fbb42829eb8e95c78659e986cebce2a9d4
brownielox <mrsmpython@gmail.com>
2017-04-18 11:23:18 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    if params[:name].blank?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
