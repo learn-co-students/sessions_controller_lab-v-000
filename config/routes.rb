87089068717db3e8f257d8eb78ce2836b11d5f06
cquinones100 <cquinones100@gmail.com>
2017-05-15 20:55:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'application#hello'
-  get '/login' => 'sessions#new'
+  get '/login', to: 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
