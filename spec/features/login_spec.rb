5ad4496cc14cf3553f7623247e06dd21b9dc07e1
Zenia <zeniavilla@hotmail.com>
2017-05-26 12:51:29 -0700

    Done.

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'login', type: :view do
+    it "redirects to the login page if input is empty" do
+        visit login_path
+        fill_in 'name', with: ''
+        click_button 'login'
+        expect(current_path).to eq '/sessions/new'
+    end
+
+    it "allows the user to log in" do        
+        visit login_path
+        fill_in 'name', with: 'gaston'
+        click_button 'login'
+        visit '/'
+        expect(page.body).to include 'gaston'
+    end
+end
