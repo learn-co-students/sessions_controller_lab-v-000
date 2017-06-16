aff34a9d35068e647698c298d404aa503968a378
Joseph Kim <josephkim0224@gmail.com>
2017-06-16 11:44:29 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].blank?
+      redirect_to "/sessions/new"
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name if session[:name]
+    redirect_to "/sessions/new"
+  end
+end
