9d09fab627d94487d63d40352501e1fcb4c7288f
Aditi Green <asaki444@gmail.com>
2017-07-21 14:13:57 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if logged_in? == nil
+    if logged_in? == nil || params[:name].empty?
       redirect_to login_path
     else
-      session[:name] = params[:name]
-      redirect_to '/'
+        session[:name] = params[:name] if params[:name] != nil
+        redirect_to root_path
     end
   end
 
