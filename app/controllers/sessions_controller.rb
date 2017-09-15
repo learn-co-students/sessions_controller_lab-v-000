4058a04d41d8d8bc088e0bbfa3f9ba62fa7612ed
skmcloughlin <skmcloughlin@gmail.com>
2017-09-15 15:43:33 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    if params[:name] == nil || params[:name] == ""
+      redirect_to '/sessions/new'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name
+      redirect_to login_path
+    else
+      redirect_to '/sessions/new'
+    end
+  end
+
+end
