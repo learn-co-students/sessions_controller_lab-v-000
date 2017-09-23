fd24ab18ba6cd6fcab04c5fecc0fd3b9b1edd4b6
Terry Blue <terryblue@comcast.net>
2017-09-23 20:55:40 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,15 @@ class SessionsController < ApplicationController
         session[:name] = params[:name]
         redirect_to '/'
       else
-        redirect_to sessions_new_url
+        binding.pry
+        redirect_to '/login'
       end
   end
 
   def destroy
-    session.delete :username
+    if session[:name]
+      session.delete :name
+    end
   end
 
 end
