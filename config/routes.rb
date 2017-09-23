82942a74af170ea96a6b85aa355fab38883aa103
Terry Blue <terryblue@comcast.net>
2017-09-23 22:36:22 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  root 'application#hello'
+  root 'sessions#new'
+  get '/hello' => 'application#hello'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
