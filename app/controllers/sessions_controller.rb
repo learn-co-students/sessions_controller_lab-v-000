870fa159034d2d7bee6dacd6e711417cee146ad6
John Fewell <fewell@gmail.com>
2017-09-28 20:12:55 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    redirect_to '/'
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  #
+  # post 'sessions/create'
+
+
+
   root 'application#hello'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
