8a45c3accb3a09ef5aab6626f98b40f15a67639b
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-10-25 12:14:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
    helper_method :invalid_name?, :login
-  
+
   def invalid_name?
     params[:name] == nil || params[:name].empty?
   end
 
   def login
     session[:name] = params[:name]
-
   end
+
+
+  def hello
+     if session[:name]
+       render :hello
+     else
+       render :'sessions/new'
+     end
+   end
 end
