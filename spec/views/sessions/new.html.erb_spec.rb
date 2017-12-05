bb9bcc7efd453ac96149ed504b5275f1daa6b693
Robin Pahlman <robin.pahlman@gmail.com>
2017-12-05 10:29:57 -0500

    Done.

---
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "home", type: :view do
+
+  it "greets a user if logged in" do
+      visit '/login'
+      fill_in "name", :with => "Ted"
+      click_button "login"
+      expect(page.body).to include "Hi, Ted"
+  end 
+  
+  it "directs user to log in if not logged in as user" do
+      session[:name] = ""
+      visit '/'
+      expect(page.body).to include "login"
+  end
+
+end
+
