3df715071721fef11b2450799bd00c388361082c
emorgan05 <emorgan05@gmail.com>
2017-12-15 14:46:10 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name] == "" || params[:name] == nil
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/login'
+  end
+end
