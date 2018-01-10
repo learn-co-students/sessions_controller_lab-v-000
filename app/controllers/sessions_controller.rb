2fdf87ca84302dc29c264a0ea9b4c3211adb14fa
Vicky Lau <vlau.nyc@gmail.com>
2018-01-10 08:24:23 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if logged_in?
+      redirect_to root_path
+    elsif !params[:name] || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
+end
