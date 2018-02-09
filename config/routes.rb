210daf7899356380b97f6c9e5732adf317b65ea6
Sam Marshall <samuel.r.marshall@gmail.com>
2018-02-08 22:58:33 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'application#hello'
-  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
