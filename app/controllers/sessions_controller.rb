7418777cd50923839da807e6edf19c4e00e12020
saurookadook <maskiella@gmail.com>
2018-03-18 19:07:48 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name] == ""
+      redirect_to sessions_new_path
+    elsif params[:name]
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name if !session[:name].nil?
+    redirect_to '/'
+  end
+
+end
