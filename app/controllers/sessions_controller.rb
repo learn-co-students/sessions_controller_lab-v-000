7e1f334c3b19e368f21c231779e04e5756fff24f
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-10 17:06:18 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+	def create 
+		if !params[:name] || params[:name] == ""
+			redirect_to login_url
+		else 
+			session[:name] = params[:name]
+			redirect_to root_url	
+		end	
+	end
+
+	def destroy 
+		if session[:name]
+			session.delete(:name)
+			redirect_to root_url
+		else 
+			redirect_to root_url	
+		end	
+	end 
+end
