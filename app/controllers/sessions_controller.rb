6ebfd46355cefa100d38ac21d834ea5123a015a5
Eva Navarror <emanao@me.com>
2018-04-17 03:13:21 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+  def create
+    if params[:name].nil? || params[:name].strip.empty?
+      redirect_to login_path
+    else
+      session[:name]= params[:name]
+      redirect_to root_path
+    end
+  end
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
