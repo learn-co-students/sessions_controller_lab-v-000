4c1a73cdd58e908ca61a33afbac271fbb6f6525d
Chris Harvey <chharvey@icloud.com>
2018-04-23 18:16:11 +0000

    Done.

--- app/controllers/sessions_conroller.rb
+++ app/controllers/sessions_conroller.rb
@@ -1,17 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-
-    return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
-    session[:name] = params[:name]
-    redirect_to controller: 'application', action: 'hello'
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to root_path
+    elsif !session[:name] || session[:name].empty?
+      redirect_to login_path
+    end
   end
 
   def destroy
     session.delete :name
-    redirect_to controller: 'application', action: 'hello'
+    redirect_to root_path
   end
-
 end
