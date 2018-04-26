128c52d3f901dd1f7f0a63bf2ec5916debb290bf
badlychadly <chadmontoya21@yahoo.com>
2018-04-26 17:35:57 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+    
+  end
+
+  # def create
+  #   if !params[:name].empty?
+  #     session[:name] = params[:name]
+
+  #   else
+  #     redirect_to login_path
+  #   end
+  # end
+end
