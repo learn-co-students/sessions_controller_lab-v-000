d6890d2074567a9fbeea9a771d2af49fc80b222b
Jul Jen <juliajen.prog@gmail.com>
2018-07-24 23:58:40 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name if !session[:name].nil?
+    redirect_to '/login'
+  end
+
+end
