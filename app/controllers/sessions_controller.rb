ceae2e1d1001377d0ea8ae674e6f1151256b4fa1
Victoria Meng <nights1ststar@protonmail.com>
2018-07-25 21:46:40 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end
+end
