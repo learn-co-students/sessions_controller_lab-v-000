0d92387549e7abe7fed63f5275561e79c97ced84
Cristina Lugo <cmlugoce@syr.edu>
2018-07-31 00:56:51 +0000

    Done.

---
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+#RSpec.describe "sessions/new",:type => :view do
+
+
+  # it 'has a form with a name field' do
+
+     #expect(rendered).to have_selector("form_tag")
+	#	expect(rendered).to match(:name)
+	#end
+#end
