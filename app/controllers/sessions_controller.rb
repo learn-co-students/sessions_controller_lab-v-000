d3f8ab0cf05874f92acbb5f092b26e2e54aae8e1
Jamie Brown <jmbrowncow@gmail.com>
2018-08-31 20:57:22 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name] && !params[:name].blank?
+      session[:name]=params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name
+    end
+    redirect_to '/login'
+  end
+
+end
