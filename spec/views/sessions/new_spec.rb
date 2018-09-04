9f03c796b8a84afba32d43e1c1431ff6f0750593
Christian Thomas <christian.thomas.2894@gmail.com>
2018-09-04 23:55:14 +0000

    Done.

--- spec/views/sessions/new_spec.rb
+++ spec/views/sessions/new_spec.rb
@@ -1,1 +1,11 @@
+require 'rails_helper'
 
+feature 'Add a login' do
+  scenario 'able to login and show user' do
+    visit '/'
+    click_link '/login'
+    fill_in 'name', with: 'Tim'
+    click_button 'login'
+    expect(page).to have_content('Tim')
+  end
+end 
