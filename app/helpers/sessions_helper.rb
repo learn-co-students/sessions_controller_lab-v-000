516fef75ece1a13d237e2a5725f6847c9df72574
theresamorelli <theresa.l.morelli@gmail.com>
2018-09-09 13:59:16 -0400

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,7 @@
 module SessionsHelper
+  def redirect_to_login_if_not_logged_in(session)
+    if !session[:username] || session[:username].empty?
+      redirect_to controller: 'sessions', action: 'new'
+    end
+  end
 end
