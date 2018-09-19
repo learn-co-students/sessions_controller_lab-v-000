1da3daa1da49aa6fd27838ed9f08285d93e59b58
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-19 00:51:08 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ActionController::Base
+  def new 
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to "/"
+  end
+end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+  
+end
