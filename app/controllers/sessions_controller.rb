b41776604ee3f7186eb3b977ead1c192d9f76d5c
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-10-01 18:30:31 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if !params[:name].blank?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to sessions_new_path
+    end
+  end
+  
+  def destroy
+    session.destroy 
+    redirect_to '/'
+  end
+end
