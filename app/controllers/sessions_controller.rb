c19bcbfb52253eb554d7db657de8f3cd8a134060
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-11 16:07:15 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    if params[:name] == nil || params[:name].empty?
+      redirect_to '/sessions/new'
+    else
+    session[:name] = params[:name]
+       redirect_to '/'
+     end
+  end
+
+  def destroy
+      render :hello
+    if session[:name] != nil
+      session.delete :name
+    end
+  end
+
+
+end
