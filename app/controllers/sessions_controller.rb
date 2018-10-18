1a658ef4be6bf43278819cacd9b24141e0283c8f
epic0448 <samuelogundiran@Yahoo.com>
2018-10-17 20:36:39 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
       if params[:name].nil? || params[:name].empty?
         redirect_to sessions_new_path
        else
+        # put there name in a session hash
         session[:name] = params[:name]
         redirect_to root_path
       end 
