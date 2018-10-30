c117b19df1a91a98573635427cfa5209a35132c0
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-30 22:23:35 +0000

    Done.

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,19 @@
+#require 'rails_helper'
+
+#RSpec.describe "homepage", :type => :view do
+  # "if the user is not logged in, the page should show them a login link." do
+   # visit '/'
+   # if !session[:name]
+     # vis
+    #  click_button 'login'
+     # expect(page.body).to include "Username"
+#    else
+      
+
+ #     expect(page.body).to include "hi, #{name}"
+  #    click_button 'logout'
+   # end
+  #end
+  
+
+#end
