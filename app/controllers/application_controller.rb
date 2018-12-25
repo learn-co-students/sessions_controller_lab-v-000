716047a89fd327966b6e6d6ae435fbb8990e6b23
Shmully Wolfson <shmullywolfson@gmail.com>
2018-12-25 11:11:09 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  
+  def hello
+    redirect_to '/sessions/new' unless session[:name]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if !params[:name] || params[:name].nil?
-      redirect_to '/login'
+    if !params[:name] || params[:name].empty?
+      redirect_to sessions_new_path
     else
       session[:name] = params[:name]
-      redirect_to '/'
+      redirect_to root_path
     end
   end
 
   def destroy
     session.delete :name
-    redirect_to '/login'
+    redirect_to login_path
   end
 end
