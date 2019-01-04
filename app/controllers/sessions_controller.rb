a81c75ceee3401da169202e21e8ebfab272a85c1
mrjak13 <justinkenney13@gmail.com>
2019-01-04 11:45:52 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    # binding.pry
+    if params[:name].present?    
+      session[:name] = params[:name]
+      redirect_to root_path
+    else
+      redirect_to "/sessions/new"
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end
+end
