6efba9211ec62c1d626d7316d26f57548e5a46a5
Joana Palero <joana.palero24@gmail.com>
2019-02-27 23:46:07 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'application#hello'
-  get '/login' => 'sessions#new'
+  get 'sessions/new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
