3dbb6c054f2e307b9704878e40c7a8f2618d05ab
Steve Albonico <steve@albonico.net>
2019-03-05 11:05:59 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
+require 'pry'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+def hello
+binding.pry
+if session[:name] == [] || session[:name] == nil
+	redirect_to '/login'
+end
+
+end
+
+
+
+
 end
