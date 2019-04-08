f6ab389440c11cfb8dda89521d918d15b4be4833
RonSala <ron.sala@gmail.com>
2019-04-07 21:42:02 -0400

    Done.

---
+++ spec/views/hello.html.erb_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# RSpec.describe "hello.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+
+#   it 'if the user is not logged in, the page should show them a login link' do
+#     session.delete :name
+#     visit '/'
+#     expect(page).to have_link('/sessions/new')
+#   end
+# end
