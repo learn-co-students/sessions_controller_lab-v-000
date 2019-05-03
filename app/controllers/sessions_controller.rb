65e0cfee6a2f20e29d43e98f9b900389f470e18f
Grace Choi <oiograceoio@gmail.com>
2019-05-03 20:29:01 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to '/login'
+    elsif session[:name]
+      redirect_to '/'
+    else
+      session[:name] = params[:name]
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
