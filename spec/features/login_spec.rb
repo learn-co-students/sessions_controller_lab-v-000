9b247876a6ee4acf6e4ba492206bcae2fd5d4cec
ricecake88 <vocalise@gmail.com>
2019-05-03 20:30:29 -0700

    Done.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,22 +1,22 @@
 require 'capybara/rails'
 require 'capybara/rspec'
 
-describe "login", type :feature do
+describe "login", type: :feature do
     before do
-        it "links to sessions/new" do
-            visit "sessions/new"
-            fill_in "name", with "Username!"
+         visit "/login"
+            fill_in "name", with: "Username!"
             click_button 'login'
+    end
+
+    describe "login" do
+        it "redirects to login page" do 
+            expect(current_path).to eq("/")
         end
-    end
 
-    it "redirects to login page" do 
-        expect(current_path).to eq("sessions/new")
-    end
-
-    it "displays the user's name" do
-        within "h1" do
-            expect(page).to have_content(name)
+        it "displays the user's name" do
+            within "h1" do
+                expect(page).to have_content("Username!")
+            end
         end
     end
 end
