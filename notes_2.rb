3a8cb10ec476dc932048c36b7dfaca1b9bd5950e
github username <github email address>
2019-05-09 16:00:28 -0400

    Done.

--- notes_2.rb
+++ notes_2.rb
@@ -1,12 +1,12 @@
-# 1. A home page or root route.
-#    * If the user is not logged in, the page should show them a login link.
-#    * If the user is logged in, the page should say, "hi, #{name}", and provide a
-#    logout link.
-# 2. A login page
-#    * Users can enter their name in a form and click 'login'. Thereafter, the app will
-#    refer to them by that name.
-#    * If the user does not enter a name, they are not logged in. Return them to the
-#    login page.
+1. A home page or root route.
+   * If the user is not logged in, the page should show them a login link.
+   * If the user is logged in, the page should say, "hi, #{name}", and provide a
+   logout link.
+2. A login page
+   * Users can enter their name in a form and click 'login'. Thereafter, the app will
+   refer to them by that name.
+   * If the user does not enter a name, they are not logged in. Return them to the
+   login page.
 
   -----------------------------------------------------------------------
    feature 'Add A Movie' do
