7aab01d89adc91c2cfd9a619aaf32d190dee12d8
AlwinaO <aoyewole2586@gmail.com>
2019-05-22 22:25:07 -0400

    Done.

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe "sessions/new.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+
+RSpec.feature "Home page" do
+  scenario "show login link if user not logged in" do
+    visit root_path
+    # include link to login
+
+    # expect(page.body).to
+  end
+
+   # this test will check if the user is logged in
+   # if yes, the page should say "hi, #{name}"
+   # provide logout link
+
 end
+
+# this test will show a form for the user to fill in  and click the login button
+# The app will refer to the user by name
+# If the user does not enter a name, they are not logged in and redirected to log in page
+# name = "Werner Brandes"
+#
+# fill_in name, with: "name"
+# click_button "login"
