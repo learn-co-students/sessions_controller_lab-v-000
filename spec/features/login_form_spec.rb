aa34a424941a275f41a8e36c343fc3f2ad8c0f46
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-02 14:39:00 -0400

    Done.

--- spec/features/login_form_spec.rb
+++ spec/features/login_form_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-describe "user logs in", type: :feature do
-
+describe "a good login", type: :feature do
   before do
+    reset_session!
     visit login_path
-    # fill_in "name", with: "Dill"
-    # click_button "login"
+    fill_in :name, with: 'Pip'
+    find('input[type="submit"]').click
   end
 
+  it "allows the user to log in with a correct username" do
+    expect(page).to have_content("Hi, Pip.")
+  end
+end
 
+describe "a bad login", type: :feature do
+  before do
+    reset_session!
+    visit login_path
+    fill_in :name, with: NIL
+    find('input[type="submit"]').click
+  end
 
+  it "redirects to login if submission is blank" do
+    expect(page).to have_content("Login")
+  end
 end
