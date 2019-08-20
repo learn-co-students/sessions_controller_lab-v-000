10dd97d66bfcfbaf012478eca6c9b1a1eb36abf2
github username <github email address>
2019-08-20 11:45:42 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ class SessionsController < ApplicationController
     def create
         
         if params[:name].nil? || params[:name].empty?
-            redirect_to "/login"
+            redirect_to login_path
         else
             
             session[:name] = params[:name]
-            redirect_to "/"
+            redirect_to root_path
         end
     end
 
     def destroy
         session.delete :name
-        redirect_to "/login"
+        redirect_to logout_path
     end
 end
