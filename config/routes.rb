8ee8ecde0e06ed4cb7946389745ad6aec800f865
dlyons8 <dlyons8@binghamton.edu>
2019-10-10 20:16:32 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new' 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  get 'sessions/create'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
   get '/login' => 'sessions#new'
