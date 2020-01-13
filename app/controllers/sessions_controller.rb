f6995d5ef0e337f18a7ba62d7ce8e23e114e2a0e
sdcro <sdcrouse91@gmail.com>
2020-01-12 18:00:30 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to root_path
+    else
+      # render "new" # If I use this, it fails the tests, and the URL looks like /login?method=post
+      redirect_to login_path # But this passes the tests, and the URL looks like /login
+    end
   end
 
   def destroy
+    if session[:name].present?
+      session.destroy
+    end
+
+    redirect_to root_path # This should happen, regardless of whether session[:name] is present.
+    # However, #destroy should only be called when session[:name] is present, so this is an edge case.
   end
 end
