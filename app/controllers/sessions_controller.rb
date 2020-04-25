19b65774a285625a1098753fbb1ed396ba856a8c
Eva Navarro <emanao@me.com>
2020-04-25 16:14:10 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
 class SessionsController < ApplicationController
   def new
-
+  end
+  def show
   end
   def create
-    if params[:name].nil? || params[:name].strip.empty?
-      redirect_to login_path
-    else
-      session[:name]=params[:name]
+    if params.include?(:name) && !params[:name].strip.empty?
+      session[:name] = params[:name]
       redirect_to root_path
+    else
+      redirect_to login_path
     end
   end
   def destroy
-    logout
+    session.clear
     redirect_to login_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'sessions#show'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
