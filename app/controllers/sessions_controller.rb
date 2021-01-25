e44af4f3512e4a0cc8ec09be2ab7936994f93320
github username <d24dust@hotmail.com>
2021-01-25 12:01:19 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def create
+    if params[:name] && params[:name] != ''
+      session[:name] = params[:name]
+      redirect_to(root_path)
+    else
+      redirect_to(login_path)
+    end
+  end
+
+  def destroy
+    session.delete(:name)
+    redirect_to(login_path)
+  end
+
+end
